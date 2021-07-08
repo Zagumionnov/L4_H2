8806ebd285960b4ed35c4873ca96d6ad849d8b9b
Dima Zahumennov <zahumennov@gmail.com>
2021-07-09 01:01:50 +0300

    add crypt

--- __init__.py
+++ __init__.py
@@ -4,7 +4,11 @@ from flask import render_template
 from cryptography.fernet import Fernet
 
 app = Flask(__name__)
+key = Fernet.generate_key()
+f = Fernet(key)
 
+
+# Take data using the request form and encrypt
 
 @app.route('/encrypt/', methods=('GET', 'POST'))
 def encrypt_page():
@@ -13,7 +17,8 @@ def encrypt_page():
         return '<form method="POST"> <input name="string"> <input type="submit"></form>'
 
     string = request.form['string']
-    return render_template('index.html', crypt='Enrypted', string=string)
+    token = f.encrypt(bytes(string, 'utf-8'))
+    return render_template('index.html', crypt='Enrypted', string=token)
 
 
 if __name__ == "__main__":
